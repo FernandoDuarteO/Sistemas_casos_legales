17a7ba73f9674fff63824ef57d6263997f5c3675
FernandoDuarteO <fernando.duarte@tecnacional.edu.ni>
2025-08-08 07:37:09 -0600

    feat: Creacion de mi migraciones de lawyer y judge

---
+++ database/migrations/2025_08_07_171626_create_judges_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('judges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('age', 3);
+            $table->string('gender', 10);
+            $table->string('identification_card', 20)->unique();
+            $table->string('phone_number', 15);
+            $table->string('email')->unique();
+            $table->string('place_birth');
+            $table->string('department');
+            $table->string('residence');
+            $table->string('country');
+            $table->string('marital_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('judges');
+    }
+};
